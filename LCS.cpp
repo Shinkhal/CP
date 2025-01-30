34c4619d6b2f808ef61a26f1ed4bb5575678ebc7
Shinkhal <shinkhalsinha@gmail.com>
2025-01-30 14:54:53 +0530

    LCS

--- LCS.cpp
+++ LCS.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define minv INT_MIN
 
 
-
+// recurssion method;
 int lcs(string& s1, string& s2,int m,int n){
 	if(n==0 || m==0)return 0;
 	if(s1[m-1]==s2[n-1]){
@@ -17,6 +17,24 @@ int lcs(string& s1, string& s2,int m,int n){
 		return max(lcs(s1,s2,m-1,n),lcs(s1,s2,m,n-1));
 	}
 
+}
+
+//dp method
+int lcs(string& s1,string& s2){
+	int m = s1.size();
+	int n= s2.size();
+
+	vector<vector<int>>dp(m+1, vector<int>(n+1,0));
+	for(int i=1;i<=m;i++){
+		for(int j=1;j<=n;j++){
+			if(s1[m-1]==s2[n-1]){
+				dp[i][j] = 1+ dp[i-1][j-1];
+			}else{
+				dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
+			}
+		}
+	}
+	return dp[m][n];
 }
 
 int main() {
@@ -30,5 +48,8 @@ int main() {
 
     int ans = lcs(s1,s2,s1.size(),s2.size());
     cout<<ans<<endl;
+
+    int ans2 = lcs(s1,s2);
+    cout<<ans2<<endl;
     return 0;
 }

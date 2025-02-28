51c1659876f773b9dd3e24321c3d45eb72b8e43c
Shinkhal <shinkhalsinha@gmail.com>
2025-02-28 23:43:52 +0530

    arrays

---
+++ rotatearray.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 100000000007
+#define gcd(a,b) __gcd(a,b)
+#define maxv INT_MAX
+#define minv INT_MIN
+void rotate(vector<int>& arr, int k){
+	int n= arr.size();
+	k = k%n;
+
+	reverse(arr.begin(), arr.end());
+	reverse(arr.begin(), arr.begin()+k);
+	reverse(arr.begin()+k, arr.end());
+
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    // Insert your code here...
+    int n;
+    cin>>n;
+
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+    	cin>>arr[i];
+    }
+    int k;
+    cin>>k;
+
+    rotate(arr,k);
+
+    for (int num : arr) cout << num << " ";
+    cout<<endl;
+
+    return 0;
+}

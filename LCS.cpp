ba7ce0c8c525df1caabc0f5c113b3dc901de06d3
Shinkhal <shinkhalsinha@gmail.com>
2025-03-08 14:21:25 +0530

    LCS leetcode

--- LCS.cpp
+++ LCS.cpp
@@ -27,7 +27,7 @@ int lcs(string& s1,string& s2){
 	vector<vector<int>>dp(m+1, vector<int>(n+1,0));
 	for(int i=1;i<=m;i++){
 		for(int j=1;j<=n;j++){
-			if(s1[m-1]==s2[n-1]){
+			if(s1[i-1]==s2[j-1]){
 				dp[i][j] = 1+ dp[i-1][j-1];
 			}else{
 				dp[i][j]= max(dp[i-1][j],dp[i][j-1]);

4ba82d83d1ca3f846e89502f30cb2bb5701ab381
Shinkhal <shinkhalsinha@gmail.com>
2025-03-23 23:07:48 +0530

    leetcode questions

--- arraySum.cpp
+++ arraySum.cpp
@@ -1,21 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int sum(int i, int arr[], int n) {
-    if (i == n) return 0;
-    return arr[i] + sum(i + 1, arr, n);
+// int sum(int i, int arr[], int n) {
+//     if (i == n) return 0;
+//     return arr[i] + sum(i + 1, arr, n);
+// }
+
+// int main() {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+    
+//     int ans = sum(0, arr, n);
+
+//     cout << ans << endl;
+//     return 0;
+// }
+
+void func(vector<int> arr, int i,vector<int>& output,int val){
+    if(i==arr.size())return;
+
+    if(arr[i]==val){
+        output.push_back(i);
+        
+    }
+    func(arr,i+1,output,val);
 }
 
-int main() {
+int main(){
     int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    
-    int ans = sum(0, arr, n);
+    cin>>n;
+    vector<int>arr(n);
 
-    cout << ans << endl;
-    return 0;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int val;
+    cin>>val;
+    vector<int>output;
+    func(arr,0,output,val);
+
+    for(int j: output){
+        cout<<j<<endl;
+    }
 }
--- kth-symbol.cpp
+++ kth-symbol.cpp
@@ -1,6 +1,57 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int countLessEqual(vector<vector<int>>& arr, int mid, int n) {
+    int count = 0;
+    int row = n - 1;
+    int col = 0;
+
+    while (row >= 0 && col < n) {
+        if (arr[row][col] <= mid) {
+            count += row + 1; 
+            col++;        
+        } else {
+            row--; 
+        }
+    }
+    return count;
+}
+
+int kthSmallest(vector<vector<int>>& arr, int k) {
+    int n = arr.size();
+    int low = arr[0][0];        
+    int high = arr[n - 1][n - 1]; 
+
+    while (low < high) { 
+        int mid = low + (high - low) / 2;
+        int count = countLessEqual(arr, mid, n);
+
+        if (count < k) {
+            low = mid + 1; 
+        } else {
+            high = mid;
+        }
+    }
+    return low;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     
+    int n, k;
+    cin >> n;
+    vector<vector<int>> arr(n, vector<int>(n));
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    
+    cin >> k;
+    cout << kthSmallest(arr, k) << "\n";
+    
+    return 0;
 }
